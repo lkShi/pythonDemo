ad9b2c56c0e02d8aa582541ff8e7c353103bfcd7
shilikun <slkgc@163.com>
2019-01-19 14:50:48 +0800

    练习

---
+++ src/demo02.07.py
@@ -0,0 +1,7 @@
+name = input("请输入你的名字")
+age = int(input("请输入你的年龄"))
+salary = float(input("请输入你的收入"))
+
+print("姓名",name)
+print("年龄",age)
+print("收入",format(salary,".1f"))
---
+++ src/demo02.08.py
@@ -0,0 +1,7 @@
+from cmath import sin #导入cmath模块中的sin函数
+#coding=utf-8sxzzzsxwasqwsdeee78u65
+print(abs(-12))
+print(sin(3.14/2))
+#四舍五入
+print(round(3.6))
+print(round(3.4))
